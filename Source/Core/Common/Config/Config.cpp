388cac425a028e34283eda836d635655de0b449a
Jonathan Alland <jonathan@Mavericks-VM.local>
2021-10-09 13:23:57 -0400

    Simplify changes to Config.cpp. Why didn't I just do this last time? (Possible reason: uh, I haven't tested if it works yet.)

--- Source/Core/Common/Config/Config.cpp
+++ Source/Core/Common/Config/Config.cpp
@@ -6,40 +6,43 @@
 #include <list>
 #include <map>
 #include <mutex>
-#include <shared_mutex>
 
 #include "Common/Config/Config.h"
 
 namespace Config
 {
     using Layers = std::map<LayerType, std::shared_ptr<Layer>>;
+    
     static Layers s_layers;
     static std::list<ConfigChangedCallback> s_callbacks;
     static u32 s_callback_guards = 0;
     static std::atomic<u64> s_config_version = 0;
     
-    Layers* GetLayers()
-{
-    {
-        return &s_layers;
-
-    const Config::LayerType layer_type = layer->GetLayer();
-    s_layers.insert_or_assign(layer_type, std::move(layer));
-    }
+    static std::mutex s_layers_rw_lock;
     
-    void AddLayer(std::unique_ptr<Layer> layer)
+    //using ReadLock = std::shared_lock<std::mutex>;
+    using WriteLock = std::unique_lock<std::mutex>;
+    
+    static void AddLayerInternal(std::shared_ptr<Layer> layer)
     {
-        s_layers[layer->GetLayer()] = std::move(layer);
+        {
+            WriteLock lock(s_layers_rw_lock);
+            
+            const Config::LayerType layer_type = layer->GetLayer();
+            s_layers.insert_or_assign(layer_type, std::move(layer));
+        }
         OnConfigChanged();
     }
     
     void AddLayer(std::unique_ptr<ConfigLayerLoader> loader)
     {
-        AddLayer(std::make_unique<Layer>(std::move(loader)));
+        AddLayerInternal(std::make_shared<Layer>(std::move(loader)));
     }
     
     std::shared_ptr<Layer> GetLayer(LayerType layer)
     {
+        //ReadLock lock(s_layers_rw_lock);
+        
         std::shared_ptr<Layer> result;
         const auto it = s_layers.find(layer);
         if (it != s_layers.end())
@@ -51,23 +54,26 @@ namespace Config
     
     void RemoveLayer(LayerType layer)
     {
-        s_layers.erase(layer);
-  }
+        {
+            WriteLock lock(s_layers_rw_lock);
+            
+            s_layers.erase(layer);
+        }
         OnConfigChanged();
-    }
-    bool LayerExists(LayerType layer)
-    {
-        return s_layers.find(layer) != s_layers.end();
     }
     
     void AddConfigChangedCallback(ConfigChangedCallback func)
     {
-        s_callbacks.emplace_back(func);
+        s_callbacks.emplace_back(std::move(func));
     }
     
     void OnConfigChanged()
     {
+        // Increment the config version to invalidate caches.
+        // To ensure that getters do not return stale data, this should always be done
+        // even when callbacks are suppressed.
         s_config_version.fetch_add(1, std::memory_order_relaxed);
+        
         if (s_callback_guards)
             return;
         
@@ -83,15 +89,23 @@ namespace Config
     // Explicit load and save of layers
     void Load()
     {
-        for (auto& layer : s_layers)
-            layer.second->Load();
+        {
+            //ReadLock lock(s_layers_rw_lock);
+            
+            for (auto& layer : s_layers)
+                layer.second->Load();
+        }
         OnConfigChanged();
     }
     
     void Save()
     {
-        for (auto& layer : s_layers)
-            layer.second->Save();
+        {
+            //ReadLock lock(s_layers_rw_lock);
+            
+            for (auto& layer : s_layers)
+                layer.second->Save();
+        }
         OnConfigChanged();
     }
     
@@ -103,22 +117,31 @@ namespace Config
     
     void Shutdown()
     {
+        WriteLock lock(s_layers_rw_lock);
+        
         s_layers.clear();
         s_callbacks.clear();
     }
     
     void ClearCurrentRunLayer()
     {
-        s_layers[LayerType::CurrentRun] = std::make_unique<Layer>(LayerType::CurrentRun);
+        WriteLock lock(s_layers_rw_lock);
+        
+        s_layers.insert_or_assign(LayerType::CurrentRun, std::make_shared<Layer>(LayerType::CurrentRun));
     }
     
     static const std::map<System, std::string> system_to_name = {
-        {System::Main, "Dolphin"},          {System::GCPad, "GCPad"},    {System::WiiPad, "Wiimote"},
-        {System::GCKeyboard, "GCKeyboard"}, {System::GFX, "Graphics"},   {System::Logger, "Logger"},
-        {System::Debugger, "Debugger"},     {System::SYSCONF, "SYSCONF"}};
-    {System::DualShockUDPClient, "DualShockUDPClient"},
-    {System::FreeLook, "FreeLook"},
-    {System::Session, "Session"}};
+        {System::Main, "Dolphin"},
+        {System::GCPad, "GCPad"},
+        {System::WiiPad, "Wiimote"},
+        {System::GCKeyboard, "GCKeyboard"},
+        {System::GFX, "Graphics"},
+        {System::Logger, "Logger"},
+        {System::Debugger, "Debugger"},
+        {System::SYSCONF, "SYSCONF"},
+        {System::DualShockUDPClient, "DualShockUDPClient"},
+        {System::FreeLook, "FreeLook"},
+        {System::Session, "Session"}};
     
     const std::string& GetSystemName(System system)
     {
@@ -151,6 +174,8 @@ namespace Config
     
     LayerType GetActiveLayerForConfig(const Location& config)
     {
+        //ReadLock lock(s_layers_rw_lock);
+        
         for (auto layer : SEARCH_ORDER)
         {
             const auto it = s_layers.find(layer);
